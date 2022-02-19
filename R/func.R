7dce8a081f13b69699e8e56b3a236ee5d860054e
Daniel Noble <daniel.noble@anu.edu.au>
2022-02-19 16:56:13 +1100

    Adding tree check function

---
+++ R/func.R
@@ -0,0 +1,20 @@
+tree_checks <- function(data, tree, dataCol, type = c("checks", "prune")){
+  type = match.arg(type)
+  # How many unique species exist in data and tree
+  Numbers <- matrix(nrow = 2, ncol = 1)
+  Numbers[1,1] <- length(unique(data[,dataCol]))
+  Numbers[2,1] <- length(tree$tip.label)
+  rownames(Numbers)<- c("Species in data:", "Species in tree:")
+  # Missing species or species not spelt correct
+  species_list1= setdiff(sort(tree$tip.label), sort(unique(data[,dataCol])))
+  species_list2= setdiff(sort(unique(data[,dataCol])), sort(tree$tip.label) )
+  if(type == "checks"){
+    return(list(SpeciesNumbers = data.frame(Numbers),
+                Species_InTree_But_NotData=species_list1,
+                Species_InData_But_NotTree=species_list2))
+  }
+  if(type == "prune"){
+    if(length(species_list2) >=1) stop("Sorry, you can only prune a tree when you have no taxa existing in the data that are not in the tree")
+    return(ape::drop.tip(tree, species_list1))
+  }
+}
