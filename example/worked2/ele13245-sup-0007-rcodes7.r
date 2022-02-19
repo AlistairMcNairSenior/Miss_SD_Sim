88403192bbcc2a897582262d9e45247100940969
Daniel Noble <daniel.noble@anu.edu.au>
2022-02-19 15:21:51 +1100

    Fixing code problems, there are alot. Tree names are not matching species names...for peat sake. Can't they just provide the code to reproduce their analysis? Sloppy.

--- example/worked2/ele13245-sup-0007-rcodes7.r
+++ example/worked2/ele13245-sup-0007-rcodes7.r
@@ -14,25 +14,19 @@ tree<- read.tree("./example/worked2/ele13245-sup-0008-phylogenys8.tre")
 prior3<-list(R=list(V=1,nu=0.002),G=list(G1=list(V=1,nu=0.002),G2=list(V=1,nu=0.002),G3=list(V=1,nu=0.002)))
 prior6<-list(R=list(V=1,nu=0.002),G=list(G1=list(V=1,nu=0.002),G2=list(V=1,nu=0.002),G3=list(V=1,nu=0.002),G4=list(V=1,nu=0.002),G5=list(V=1,nu=0.002),G6=list(V=1,nu=0.002)))
 
-#taxonomy model with fixed effects
-taxonomymodel <- MCMCglmm(yi~ density + species + spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
-                            random=~Group+Year+Focal_insect+F_Order+F_Family+F_Genus, mev=dat$vi, data=dat,prior=prior6,
-                            family="gaussian", nitt=1000000, thin=100, burnin=100000)
-
-
 #set up for phylogeny model
 
 #create copy of data file to use with tree
 a1 <- dat
 
 #align focal species with branches of phylogenetic tree
-tree1<-drop.tip(tree,tree$tip.label[which(tree$tip.label%in%a1$`Focal insect`==FALSE)])
+tree1<-drop.tip(tree,tree$tip.label[which(tree$tip.label%in%a1$Focal_insect==FALSE)])
 check.species<-function(x) {any(x==tree1$tip.label)}
-a1 <- a1[sapply(a1[,"Focal insect"],check.species),]
+a1 <- a1[sapply(a1[,"Focal_insect"],check.species),]
 INtree <- inverseA(tree1,nodes="TIPS")
 
 #phylogeny model with fixed effects
-phylogenymodel<-MCMCglmm(yi ~ density + species + spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
+phylogenymodel<-MCMCglmm(yi_g ~ spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
                          random=~Group+Year+Focal_insect, data=a1, mev=a1$vi, ginverse=list(Focal_insect=INtree$Ainv),
                          prior=prior3, nitt=1000000, thin=100, burnin=500000)
 
