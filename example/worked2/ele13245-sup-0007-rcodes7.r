f13c1a011370e5b1a1275ecb73cfb3c1d1ee7513
Daniel Noble <daniel.noble@anu.edu.au>
2022-02-20 07:46:28 +1100

    Fixed code and added a check to make sure that species names match. May add to tree check function to subset both data and tree at same time.

--- example/worked2/ele13245-sup-0007-rcodes7.r
+++ example/worked2/ele13245-sup-0007-rcodes7.r
@@ -13,6 +13,7 @@ tree<- read.tree("./example/worked2/ele13245-sup-0008-phylogenys8.tre")
 
 tree_checks(data = dat, tree = tree, dataCol = "Focal_insect", type = "checks")
 
+
 #priors
 prior3<-list(R=list(V=1,nu=0.002),G=list(G1=list(V=1,nu=0.002),G2=list(V=1,nu=0.002),G3=list(V=1,nu=0.002)))
 prior6<-list(R=list(V=1,nu=0.002),G=list(G1=list(V=1,nu=0.002),G2=list(V=1,nu=0.002),G3=list(V=1,nu=0.002),G4=list(V=1,nu=0.002),G5=list(V=1,nu=0.002),G6=list(V=1,nu=0.002)))
@@ -26,10 +27,14 @@ a1 <- dat
 tree1<-drop.tip(tree,tree$tip.label[which(tree$tip.label%in%a1$Focal_insect==FALSE)])
 check.species<-function(x) {any(x==tree1$tip.label)}
 a1 <- a1[sapply(a1[,"Focal_insect"],check.species),]
+
+# Quick check to make sure that pruning has been done correctly.
+tree_checks(data = a1, tree = tree1, dataCol = "Focal_insect", type = "checks")
+
 INtree <- inverseA(tree1,nodes="TIPS")
 
 #phylogeny model with fixed effects
-phylogenymodel<-MCMCglmm(yi_g ~ spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
+phylogenymodel<-MCMCglmm(yi_g ~ Spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
                          random=~Group+Year+Focal_insect, data=a1, mev=a1$vi, ginverse=list(Focal_insect=INtree$Ainv),
                          prior=prior3, nitt=1000000, thin=100, burnin=500000)
 
