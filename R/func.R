54f479714fa88678fcd749cd527e8b73b84aef46
Daniel Noble <daniel.noble@anu.edu.au>
2022-02-20 21:16:32 +1100

    New function for generating missing SD

--- R/func.R
+++ R/func.R
@@ -1,13 +1,13 @@
-tree_checks <- function(data, tree, dataCol, type = c("checks", "prune")){
+tree_checks <- function(data, tree, species_name_col, type = c("checks", "prune")){
   type = match.arg(type)
   # How many unique species exist in data and tree
   Numbers <- matrix(nrow = 2, ncol = 1)
-  Numbers[1,1] <- length(unique(data[,dataCol]))
+  Numbers[1,1] <- length(unique(data[,species_name_col]))
   Numbers[2,1] <- length(tree$tip.label)
   rownames(Numbers)<- c("Species in data:", "Species in tree:")
   # Missing species or species not spelt correct
-  species_list1= setdiff(sort(tree$tip.label), sort(unique(data[,dataCol])))
-  species_list2= setdiff(sort(unique(data[,dataCol])), sort(tree$tip.label) )
+  species_list1= setdiff(sort(tree$tip.label), sort(unique(data[,species_name_col])))
+  species_list2= setdiff(sort(unique(data[,species_name_col])), sort(tree$tip.label) )
   if(type == "checks"){
     return(list(SpeciesNumbers = data.frame(Numbers),
                 Species_InTree_But_NotData=species_list1,
@@ -17,4 +17,11 @@ tree_checks <- function(data, tree, dataCol, type = c("checks", "prune")){
     if(length(species_list2) >=1) stop("Sorry, you can only prune a tree when you have no taxa existing in the data that are not in the tree")
     return(ape::drop.tip(tree, species_list1))
   }
+}
+
+
+gen.miss <- function(data, missVar, missCol2, n_miss){
+  data[sample(rownames(data), n_miss), missVar] <- NA
+  data[is.na(data[,missVar]), missCol2] <- NA
+  return(data)
 }
