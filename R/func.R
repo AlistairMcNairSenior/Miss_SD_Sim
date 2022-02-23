a4362084132a111274521c632f5f5d1956f21dfc
Daniel Noble <daniel.noble@anu.edu.au>
2022-02-23 13:46:34 +1100

    writing function for calculating cv

--- R/func.R
+++ R/func.R
@@ -25,3 +25,37 @@ gen.miss <- function(data, missVar, missCol2, n_miss){
   data[is.na(data[,missVar]), missCol2] <- NA
   return(data)
 }
+
+
+cv_avg <- function(cv, n, group, data){
+  b_grp_cv_data <- data                                      %>%
+              group_by({{group}})                            %>%
+              mutate(   w_CV = weighted.mean({{cv}}, {{n}},
+                                             na.rm = TRUE),
+                     n_total = sum({{n}}, na.rm = TRUE))     %>%
+              ungroup(.)                                     %>%
+              mutate(b_CV = weighted.mean(w_CV, n_total))
+
+  return(data.frame(b_grp_cv_data))
+
+}
+
+
+# test data for cv_avg function
+library(tidyverse)
+set.seed(76)
+x1 = rnorm(16, 6, 1)
+test_dat <- data.frame(stdy = rep(c(1,2,3,4), each = 4),
+                          x = x1,
+                         sd = exp(log(x1)*1.5 + rnorm(16, 0, sd = 0.10)),
+                          n = rpois(16, 15))
+test_dat <- test_dat %>% mutate(cv = x / sd)
+plot(sd ~ x, data = test_dat)
+plot(log(sd) ~ log(x), data = test_dat)
+
+t <- cv_avg(cv, n, stdy, data =  test_dat)
+
+# Now generate some missing data
+t2 <- gen.miss(test_dat, "sd", "cv", 6)
+
+t2_cv <- cv_avg(cv, n, stdy, data =  t2)
