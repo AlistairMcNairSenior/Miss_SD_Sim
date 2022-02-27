5f998d467a7039353fa0bf8e4247fc6bbff1af7b
Daniel Noble <daniel.noble@anu.edu.au>
2022-02-27 16:40:07 +1100

    Cleaning code. 1) check that author models match metafor models. Pretty much, yes; 2) Clean up code; 3) Add in example 2 code to reproduce abundance analysis in Fugure 1. These results match with metafor

--- example/worked2/ele13245-sup-0007-rcodes7.r
+++ example/worked2/ele13245-sup-0007-rcodes7.r
@@ -1,7 +1,12 @@
-#run libraries
+############################
+# Load libraries
+###########################
 pacman::p_load(ape, metafor, MCMCglmm, tidyverse, readxl)
 source("./R/func.R")
 
+############################
+# Loading and sorting data out
+###########################
 #load data
 dat  <- data.frame(read_excel("./example/worked2/ele13245-sup-0003-metas3.xlsx"))
 
@@ -10,23 +15,17 @@ dat$Experimental_standard_deviation <- as.numeric(dat$Experimental_standard_devi
 dat$Control_standard_deviation <- as.numeric(dat$Control_standard_deviation)
 
 #remove rows with missing data
-dat <- na.omit(dat)
-
-# Generate missing data in SD's at the effect size level. If you're missing one SD then your missing for control and experimental
-dat_missSD <- gen.miss(dat, missVar = "Experimental_standard_deviation",
-                           missCol2 = "Control_standard_deviation",
-                             n_miss = 0.2*nrow(dat))
+    dat <- na.omit(dat)
+dat$obs <- 1:nrow(dat) # Needed for metafor to make model equivalent to MCMCglmm
 
 #load phylogenetic tree
 tree<- read.tree("./example/worked2/ele13245-sup-0008-phylogenys8.tre")
 
-tree_checks(data = dat, tree = tree, dataCol = "Focal_insect", type = "checks")
+tree_checks(data = dat, tree = tree, species_name_col = "Focal_insect", type = "checks")
 
-
-#priors
-prior3<-list(R=list(V=1,nu=0.002),G=list(G1=list(V=1,nu=0.002),G2=list(V=1,nu=0.002),G3=list(V=1,nu=0.002)))
-prior6<-list(R=list(V=1,nu=0.002),G=list(G1=list(V=1,nu=0.002),G2=list(V=1,nu=0.002),G3=list(V=1,nu=0.002),G4=list(V=1,nu=0.002),G5=list(V=1,nu=0.002),G6=list(V=1,nu=0.002)))
-
+##################
+# Checks with authors code
+##################
 #set up for phylogeny model
 
 #create copy of data file to use with tree
@@ -40,13 +39,66 @@ a1 <- a1[sapply(a1[,"Focal_insect"],check.species),]
 # Quick check to make sure that pruning has been done correctly.
 tree_checks(data = a1, tree = tree1, species_name_col = "Focal_insect", type = "checks")
 
-INtree <- inverseA(tree1,nodes="TIPS")
+INtree <- inverseA(tree1)  # Metafor takes a correlation matrix
+INtree2 <- vcv(tree1, corr = TRUE)  # Metafor takes a correlation matrix
 
 #phylogeny model with fixed effects
-phylogenymodel<-MCMCglmm(yi_g ~ Spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
-                         random=~Group+Year+Focal_insect, data=a1, mev=a1$vi, ginverse=list(Focal_insect=INtree$Ainv),
-                         prior=prior3, nitt=1000000, thin=100, burnin=500000)
+    rerun=FALSE
+    if(rerun){
+      #priors
+      prior3<-list(R=list(V=1,nu=0.002),
+                   G=list(G1=list(V=1,nu=0.002),
+                          G2=list(V=1,nu=0.002),
+                          G3=list(V=1,nu=0.002)))
+      prior6<-list(R=list(V=1,nu=0.002),
+                   G=list(G1=list(V=1,nu=0.002),
+                          G2=list(V=1,nu=0.002),
+                          G3=list(V=1,nu=0.002),
+                          G4=list(V=1,nu=0.002),
+                          G5=list(V=1,nu=0.002),
+                          G6=list(V=1,nu=0.002)))
 
-saveRDS(phylogenymodel, "./example/worked2/phylogenymodel")
+      phylogenymodel<-MCMCglmm(yi_g ~ Spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild,
+                             random=~Group+Year+Focal_insect, data=a1, mev=a1$vi, ginverse=list(Focal_insect=INtree$Ainv),
+                             prior=prior3, nitt=1000000, thin=100, burnin=500000)
+
+    saveRDS(phylogenymodel, "./example/worked2/phylogenymodel")} else {phylogenymodel <- readRDS("./example/worked2/phylogenymodel")}
 
 
+    ## Metafor models. First, check it matches autors code.
+    phylogenymodel_mv <- rma.mv(yi_g ~ Spatial_separation + temporal_separation + F_Guild + C_Guild + F_Guild:C_Guild, V = vi, random=list(~1|Group, ~1|Year, ~1|Focal_insect, ~1|obs), R = list(Focal_insect = INtree2), data = a1)
+
+
+#######################
+    # Example 2
+#######################
+# Intercept only model. for Abundance fitness ONLY. The authors state: "We first estimated the average impact of insect interaction on each fitness component by fitting linear models in which the intercept was the only fixed effect." Table 1 provides the fitness components. Abundance has the most data.
+    a2 <- dat %>% filter(Fitness_component == "Abundance") # Doesn't quite match Table 1, but does match figure 1, and even exclusion N = 311, which matches.
+
+    # Prune tree
+    #align focal species with branches of phylogenetic tree
+    tree2_meta<-drop.tip(tree,
+                         tree$tip.label[which(tree$tip.label%in%a2$Focal_insect==FALSE)])
+    check.species<-function(x) {any(x==tree2_meta$tip.label)}
+    a2 <- a2[sapply(a2[,"Focal_insect"],check.species),]
+
+    tree_checks(data = a2, tree = tree2_meta, species_name_col = "Focal_insect", type = "checks")
+
+    phylo <- vcv(tree2_meta, corr = TRUE)
+
+
+#####################
+# Complete data model
+#####################
+
+    complete_mv <- rma.mv(yi_g ~ 1, V = vi,
+                          random=list(~1|Group, ~1|Year, ~1|Focal_insect, ~1|obs),
+                          R = list(Focal_insect = phylo), data = a2)
+
+#####################
+# Generate missing data
+#####################
+    # Generate missing data in SD's at the effect size level. If you're missing one SD then your missing for control and experimental
+    a2_missSD <- gen.miss(a2, missVar = "Experimental_standard_deviation",
+                           missCol2 = "Control_standard_deviation",
+                           n_miss = 0.2*nrow(a2))
