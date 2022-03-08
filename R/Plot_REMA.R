05fca1a0537a4ad13d8559a89d78bda6efb0df20
Alistair senior <alistairsenior@Alistairs-Mac-mini.local>
2022-03-08 13:57:31 +1100

    update to plot for rema

--- R/Plot_REMA.R
+++ R/Plot_REMA.R
@@ -20,7 +20,7 @@ library(ggbeeswarm)
 library(ggcorrplot)
 
 # Load the results
-load("Rdata/agg_results_REMA.Rdata")
+load("Rdata/agg_results_rema.Rdata")
 
 ########################################################################
 ############## Averaging each parameter set ############################
