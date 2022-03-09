d8fd91336345f6caa174f00831031b65dcfc5fb3
Alistair senior <alistairsenior@alistairs-ipro.staff.sydney.edu.au>
2022-03-09 11:33:48 +1100

    results from rema sim

--- R/Plot_REMA.R
+++ R/Plot_REMA.R
@@ -9,7 +9,7 @@
 rm(list=ls())
 
 # Set the working directory
-wd<-"/Users/alistairsenior/OneDrive - The University of Sydney (Staff)/Nakagawa_Ecology_Missing_SD/Miss_SD_Sim" # MacMini Home
+wd<-"/Users/alistairsenior/OneDrive - The University of Sydney (Staff)/Nakagawa_Ecology_Missing_SD/Miss_SD_Sim" # MacMini Home and work iMac
 setwd(wd)
 
 # Load the relevant libraries, and header
@@ -81,7 +81,7 @@ E<-ggplot(dat1.2, aes(x=log(range_bias, base=10), y=as.factor(sd_study_sd), fill
 	theme_bw() +
 	xlab("log10 Range of Bias lnRR") +
 	ylab(expression(sigma[s])) + 
-	theme(axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), axis.title.y=element_text(size=15), legend.position=c(0.8, 0.2), legend.text=element_text(size=15), legend.title=element_text(size=15), plot.title=element_text(size=15)) + guides(fill=guide_legend(title="Study Sample Size"))	
+	theme(axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), axis.title.y=element_text(size=15), legend.position=c(0.8, 0.2), legend.text=element_text(size=15), legend.title=element_text(size=15), plot.title=element_text(size=15)) + guides(fill=guide_legend(title="Study Sample Size")) + xlim(-1.75, 1)	
 
 
 
@@ -97,27 +97,27 @@ dev.off()
 A<-ggplot(data=agg_results, aes(x=coverage, y=Method, col=Method, fill=Method)) +
 	geom_violin() + 
 	geom_beeswarm(groupOnX=F, size=0.05, cex=0.25, col="black") + 
-	xlim(0.9, 1) +
+	xlim(0.85, 1) +
 	xlab("Coverage (95% CI)") + ylab("") + theme_bw() + 
 	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), plot.title=element_text(size=15)) +
 	geom_vline(xintercept=0.95, col="dark grey", size=0.5)
 	
-B<-ggplot(data=dat1.1, aes(x=as.factor(tau2), y=coverage, fill=as.factor(icc_study))) +
-	geom_violin() + 
+B<-ggplot(data=dat1.1, aes(x=as.factor(tau2), y=coverage)) +
+	geom_violin(fill="lightgoldenrod3") + 
 	theme_bw() + 
 	ylim(0.9, 1) +
 	xlab(expression(italic(T)^2)) + ylab("Coverage (95% CI)") +
-	theme(legend.position=c(0.85, 0.875), axis.text.x=element_text(size=12), axis.title.x=element_text(size=15), axis.text.y=element_text(size=12), axis.title.y=element_text(size=15), legend.title=element_text(size=15), plot.title=element_text(size=15), plot.subtitle=element_text(size=15), legend.text=element_text(size=15)) +
-	geom_hline(yintercept=0.95, col="dark grey") + guides(fill=guide_legend(title="Simulated ICC")) +
+	theme(axis.text.x=element_text(size=12), axis.title.x=element_text(size=15), axis.text.y=element_text(size=12), axis.title.y=element_text(size=15), plot.title=element_text(size=15), plot.subtitle=element_text(size=15)) +
+	geom_hline(yintercept=0.95, col="dark grey") +
 	scale_x_discrete(labels=c(expression(9e-06~~or~~over(italic(T), lnRR)==0.01), expression(0.09~~or~~over(italic(T), lnRR)==1))) + labs(subtitle="Method 1A")
 
-C<-ggplot(data=dat1.2, aes(x=as.factor(tau2), y=coverage, fill=as.factor(icc_study))) +
-	geom_violin() + 
+C<-ggplot(data=dat1.2, aes(x=as.factor(tau2), y=coverage)) +
+	geom_violin(fill="aquamarine3") + 
 	theme_bw() + 
-	xlab(expression(italic(T)^2)) + ylab("Coverage (95% CI)") +
 	ylim(0.9, 1) +
-	theme(legend.position=c(0.85, 0.875), axis.text.x=element_text(size=12), axis.title.x=element_text(size=15), axis.text.y=element_text(size=12), axis.title.y=element_text(size=15), legend.title=element_text(size=15), plot.title=element_text(size=15), plot.subtitle=element_text(size=15), legend.text=element_text(size=15)) +
-	geom_hline(yintercept=0.95, col="dark grey") + guides(fill=guide_legend(title="Simulated ICC")) +
+	xlab(expression(italic(T)^2)) + ylab("Coverage (95% CI)") +
+	theme(axis.text.x=element_text(size=12), axis.title.x=element_text(size=15), axis.text.y=element_text(size=12), axis.title.y=element_text(size=15), plot.title=element_text(size=15), plot.subtitle=element_text(size=15)) +
+	geom_hline(yintercept=0.95, col="dark grey") +
 	scale_x_discrete(labels=c(expression(9e-06~~or~~over(italic(T), lnRR)==0.01), expression(0.09~~or~~over(italic(T), lnRR)==1))) + labs(subtitle="Method 1B")
 
 pdf("MS/fig/Coverage_REMA.pdf", height=7, width=18)
@@ -128,15 +128,15 @@ dev.off()
 	
 # Analysis of bias in Tau2
 	
-A<-ggplot(data=agg_results, aes(x=mean_bias_tau2_lnR, y=Method, col=Method, fill=Method)) +
+A<-ggplot(data=agg_results, aes(x=median_bias_tau2_lnR, y=Method, col=Method, fill=Method)) +
 	geom_violin() + 
 	geom_beeswarm(groupOnX=F, size=0.05, cex=0.25, col="black") + 
-	xlim(-6, 6) +
+	xlim(-15, 6) +
 	xlab("Bias in Heterogeneity (log Ratio)") + ylab("") + theme_bw() + 
 	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.x=element_text(size=12), axis.text.y=element_text(size=15), plot.title=element_text(size=15)) +
 	geom_vline(xintercept=0, col="dark grey", size=0.5)
 
-B<-ggplot(data=agg_results, aes(x=as.factor(tau2), y= mean_bias_tau2_lnR, fill=Method)) +
+B<-ggplot(data=agg_results, aes(x=as.factor(tau2), y= median_bias_tau2_lnR, fill=Method)) +
 	geom_boxplot() + theme_bw() + 
 	xlab(expression(italic(T)^2)) + ylab("Bias in Heterogeneity (log Ratio)") +
 	theme(legend.position="none", axis.text.x=element_text(size=12), axis.title.x=element_text(size=15), axis.text.y=element_text(size=12), axis.title.y=element_text(size=15), plot.title=element_text(size=15)) +
