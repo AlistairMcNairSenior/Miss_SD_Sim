cd8be95817b77c154e4fcfa0808348550247cea9
Alistair senior <alistairsenior@alistairs-ipro.staff.sydney.edu.au>
2022-03-31 11:55:21 +1100

    updating figures for median

--- R/Plot_MLMA.R
+++ R/Plot_MLMA.R
@@ -36,24 +36,16 @@ agg_results$Method[which(agg_results$Method == "Method 1.1")]<-"Method 1A"
 agg_results$Method[which(agg_results$Method == "Method 1.2")]<-"Method 1B"
 agg_results$Method[which(agg_results$Method == "Complete Data")]<-"Full Data"
 
-A<-ggplot(data=agg_results, aes(x=mean_bias, y=Method, col=Method, fill=Method)) +
+A<-ggplot(data=agg_results, aes(x=median_bias, y=Method, col=Method, fill=Method)) +
 	geom_violin() + 
 	geom_beeswarm(groupOnX=F, size=0.05, cex=0.25, col="black") + 
-	xlim(-0.011, 0.008) +
+	xlim(-0.0025, 0.008) +
 	xlab("Bias lnRR") + ylab("") + theme_bw() + 
 	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), plot.title=element_text(size=15)) +
 	geom_vline(xintercept=0, col="dark grey", size=0.5)
 
 
-# # B<-ggplot(data=agg_results, (aes(x=as.factor(proportion_lost), y=mean_bias, fill=Method, col=Method))) + 
-	# geom_violin() +
-	# theme_bw() +
-	# geom_hline(yintercept=0, col="grey") +
-	# xlab("Proportion Missing") + ylab("Bias lnRR") +
-	# theme(legend.position="none", axis.title.x=element_text(size=15), axis.title.y=element_text(size=15))
-
-
-long<-agg_results[,c("Method", "mean_bias", "code")]
+long<-agg_results[,c("Method", "median_bias", "code")]
 wide<-reshape(long, direction="wide", idvar="code", timevar="Method")
 cor.mat<-cor(wide[,-1])
 rownames(cor.mat)<-c("Full", "Method 1A", "Method 1B", "Method 2", "Method 3")
@@ -62,7 +54,7 @@ colnames(cor.mat)<-c("Full", "Method 1A", "Method 1B", "Method 2", "Method 3")
 B<-ggcorrplot(cor.mat, lab=T, show.legend=FALSE, type="lower") + theme(plot.title=element_text(size=15))
 
 # Difference in the bias
-df<-data.frame(delta=abs(wide[,"mean_bias.Method 1A"]) - abs(wide[,"mean_bias.Method 1B"]))
+df<-data.frame(delta=abs(wide[,"median_bias.Method 1A"]) - abs(wide[,"median_bias.Method 1B"]))
 C<-ggplot(df, aes(x=delta)) + geom_histogram(bins=20) + theme_bw() + geom_vline(xintercept=0, col="red") +
 	xlab("Diff. |Bias| Meths 1A & 1B") + ylab("Frequency")
 
@@ -129,22 +121,22 @@ dev.off()
 	
 # Analysis of bias in Tau2
 	
-A<-ggplot(data=agg_results, aes(x=mean_bias_tau2_lnR, y=Method, col=Method, fill=Method)) +
+A<-ggplot(data=agg_results, aes(x=median_bias_tau2_lnR, y=Method, col=Method, fill=Method)) +
 	geom_violin() + 
 	geom_beeswarm(groupOnX=F, size=0.05, cex=0.25, col="black") + 
-	xlim(-6, 6) +
+	xlim(-1, 6) +
 	xlab("Bias in Heterogeneity (log Ratio)") + ylab("") + theme_bw() + 
 	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.x=element_text(size=12), axis.text.y=element_text(size=15), plot.title=element_text(size=15)) +
 	geom_vline(xintercept=0, col="dark grey", size=0.5)
 
-B<-ggplot(data=agg_results, aes(x=as.factor(tau2), y= mean_bias_tau2_lnR, fill=Method)) +
+B<-ggplot(data=agg_results, aes(x=as.factor(tau2), y=median_bias_tau2_lnR, fill=Method)) +
 	geom_boxplot() + theme_bw() + 
 	xlab(expression(italic(T)^2)) + ylab("Bias in Heterogeneity (log Ratio)") +
 	theme(legend.position="none", axis.text.x=element_text(size=12), axis.title.x=element_text(size=15), axis.text.y=element_text(size=12), axis.title.y=element_text(size=15), plot.title=element_text(size=15)) +
 	geom_hline(yintercept=0, col="dark grey") +
 	scale_x_discrete(labels=c(expression(9e-06~~or~~over(italic(T), lnRR)==0.01), expression(0.09~~or~~over(italic(T), lnRR)==1)))
 
-C<-ggplot(data=agg_results, aes(x=mean_bias_ICC, y=Method, col=Method, fill=Method)) +
+C<-ggplot(data=agg_results, aes(x=median_ICC, y=Method, col=Method, fill=Method)) +
 	geom_violin() + 
 	geom_beeswarm(groupOnX=F, size=0.05, cex=0.25, col="black") + 
 	xlim(-0.5, 0.75) +
@@ -152,7 +144,7 @@ C<-ggplot(data=agg_results, aes(x=mean_bias_ICC, y=Method, col=Method, fill=Meth
 	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.x=element_text(size=12), axis.text.y=element_text(size=15), plot.title=element_text(size=15)) +
 	geom_vline(xintercept=0, col="dark grey", size=0.5)
 		
-D<-ggplot(data=dat1.2, aes(x=as.factor(tau2), y= mean_bias_ICC, fill=as.factor(icc_study))) +
+D<-ggplot(data=dat1.2, aes(x=as.factor(tau2), y=median_ICC, fill=as.factor(icc_study))) +
 	geom_violin() + 
 	theme_bw() + 
 	xlab(expression(italic(T)^2)) + ylab("Bias in ICC") +
