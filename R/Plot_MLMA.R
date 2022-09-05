9996d344010a7d32ab6845f8556b136dfe8c8dd6
Alistair senior <alistairsenior@192-168-1-104.tpgi.com.au>
2022-09-05 21:47:32 +1000

    updating figs

--- R/Plot_MLMA.R
+++ R/Plot_MLMA.R
@@ -18,9 +18,13 @@ library(plyr)
 library(gridExtra)
 library(ggbeeswarm)
 library(ggcorrplot)
+library(ggthemes)
 
 # Load the results
 load("Rdata/agg_results_mlma.Rdata")
+
+# Params
+title_size<-20
 
 ########################################################################
 ############## Averaging each parameter set ############################
@@ -32,51 +36,62 @@ head(agg_results)
 agg_results$range_bias<-agg_results$max_bias - agg_results$min_bias
 
 # Rename 1.1 and 1.2 to 1.A and 1.B, and complete to full
-agg_results$Method[which(agg_results$Method == "Method 1.1")]<-"Method 1A"
-agg_results$Method[which(agg_results$Method == "Method 1.2")]<-"Method 1B"
-agg_results$Method[which(agg_results$Method == "Complete Data")]<-"Full Data"
+# agg_results$Method_old<-agg_results$Method
+# agg_results$Method[which(agg_results$Method == "Method 1.1")]<-"Missing Cases"
+# agg_results$Method[which(agg_results$Method == "Method 1.2")]<-"All Cases"
+# agg_results$Method[which(agg_results$Method == "Method 2")]<-"Multiplicative"
+# agg_results$Method[which(agg_results$Method == "Method 3")]<-"Hybrid"
+# agg_results$Method[which(agg_results$Method == "Complete Data")]<-"Full Data"
+method_cols<-colorblind_pal()(8)[c(2,3,4,7,8)]
+other_cols<-colorblind_pal()(8)[c(1,5)]
 
-A<-ggplot(data=agg_results, aes(x=median_bias, y=Method, col=Method, fill=Method)) +
+A<-ggplot(data=agg_results, aes(y=median_bias, x=Method, col=Method, fill=Method)) + 
 	geom_violin() + 
-	geom_beeswarm(groupOnX=F, size=0.05, cex=0.25, col="black") + 
-	xlim(-0.0025, 0.008) +
-	xlab("Bias lnRR") + ylab("") + theme_bw() + 
-	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), plot.title=element_text(size=15)) +
-	geom_vline(xintercept=0, col="dark grey", size=0.5)
+	geom_beeswarm(groupOnX=T, size=0.6, cex=0.2, col="black", alpha=1/3, shape=1) + 
+	ylim(-0.0025, 0.008) +
+	ylab("Bias lnRR") + xlab("") + theme_bw() + 
+	theme(legend.position="none", axis.title.y=element_text(size=15), axis.text.y=element_text(size=15), axis.text.x=element_text(size=15), plot.title=element_text(size=title_size)) +
+	geom_vline(xintercept=0, col="dark grey", size=0.5) + 
+	scale_x_discrete(labels=c("Full Data", "Miss.
+Cases", "All
+Cases", "Multiplic.", "Hybrid")) + scale_color_manual(values=method_cols) + scale_fill_manual(values=method_cols)
 
 
 long<-agg_results[,c("Method", "median_bias", "code")]
 wide<-reshape(long, direction="wide", idvar="code", timevar="Method")
 cor.mat<-cor(wide[,-1])
-rownames(cor.mat)<-c("Full", "Method 1A", "Method 1B", "Method 2", "Method 3")
-colnames(cor.mat)<-c("Full", "Method 1A", "Method 1B", "Method 2", "Method 3")
+rownames(cor.mat)<-c("Full Data", "Miss. Cases", "All Cases", "Multiplicative", "Hybrid")
+colnames(cor.mat)<-c("Full Data", "Miss. Cases", "All Cases", "Multiplicative", "Hybrid")
 
-B<-ggcorrplot(cor.mat, lab=T, show.legend=FALSE, type="lower") + theme(plot.title=element_text(size=15))
+B<-ggcorrplot(cor.mat, lab=T, show.legend=FALSE, type="lower") + theme(plot.title=element_text(size=title_size), axis.text.x=element_text(size=15),  axis.text.y=element_text(size=15))
 
 # Difference in the bias
-df<-data.frame(delta=abs(wide[,"median_bias.Method 1A"]) - abs(wide[,"median_bias.Method 1B"]))
+df<-data.frame(delta=abs(wide[,"median_bias.Missing Cases"]) - abs(wide[,"median_bias.All Cases"]))
 C<-ggplot(df, aes(x=delta)) + geom_histogram(bins=20) + theme_bw() + geom_vline(xintercept=0, col="red") +
-	xlab("Diff. |Bias| Meths 1A & 1B") + ylab("Frequency")
+	xlab("Diff. |Bias| Miss. & All Cases") + ylab("Frequency") + 
+	theme(axis.title.y=element_text(size=15), axis.text.y=element_text(size=15), axis.text.x=element_text(size=12), axis.title.x=element_text(size=12), plot.title=element_text(size=title_size))
 
-D<-ggplot(data=agg_results, aes(x=log(range_bias, base=10), y=Method, col=Method, fill=Method)) +
+D<-ggplot(data=agg_results, aes(y=log(range_bias, base=10), x=Method, col=Method, fill=Method)) +
 	geom_violin() + 
-	geom_beeswarm(groupOnX=FALSE, size=0.05, cex=0.25, col="black") + 
-	xlab("log10 Range of Bias lnRR") + ylab("") + theme_bw() + 
-	theme(legend.position="none", axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), plot.title=element_text(size=15))
+	geom_beeswarm(groupOnX=T, size=2, cex=0.5, col="black", alpha=1/3, shape=1) + 
+	ylab("log10 Range of Bias lnRR") + xlab("") + theme_bw() + 
+	theme(legend.position="none", axis.title.y=element_text(size=15), axis.text.y=element_text(size=15), axis.text.x=element_text(size=15), plot.title=element_text(size=title_size)) + 
+	scale_x_discrete(labels=c("Full Data", "Miss. 
+Cases", "All 
+Cases", "Multiplic.", "Hybrid")) + scale_color_manual(values=method_cols) + scale_fill_manual(values=method_cols)
+
 
 # Get the data for Methods 1.A & 1.B
-dat1.1<-agg_results[which(agg_results$Method=="Method 1A"),]
-dat1.2<-agg_results[which(agg_results$Method=="Method 1B"),]
+dat1.1<-agg_results[which(agg_results$Method=="Method 1.1"),]
+dat1.2<-agg_results[which(agg_results$Method=="Method 1.2"),]
 
-E<-ggplot(dat1.2, aes(x=log(range_bias, base=10), y=as.factor(sd_study_sd), fill=as.factor(n_study_mu))) +
+E<-ggplot(dat1.2, aes(y=log(range_bias, base=10), x=as.factor(sd_study_sd), fill=as.factor(n_study_mu))) +
 	geom_violin() +
 	geom_point(size=-1, col="grey") +
 	theme_bw() +
-	xlab("log10 Range of Bias lnRR") +
-	ylab(expression(sigma[s])) + 
-	theme(axis.title.x=element_text(size=15), axis.text.y=element_text(size=15), axis.title.y=element_text(size=15), legend.position=c(0.8, 0.2), legend.text=element_text(size=15), legend.title=element_text(size=15), plot.title=element_text(size=15)) + guides(fill=guide_legend(title="Study Sample Size"))	
-
-
+	ylab("log10 Range of Bias lnRR") +
+	xlab(expression(sigma[s])) + 
+	theme(axis.title.x=element_text(size=20), axis.text.y=element_text(size=15), axis.title.y=element_text(size=15), axis.text.x=element_text(size=15), legend.position=c(0.2, 0.8), legend.text=element_text(size=15), legend.title=element_text(size=15), plot.title=element_text(size=title_size)) + guides(fill=guide_legend(title="Study Sample Size")) + scale_color_manual(values=other_cols) + scale_fill_manual(values=other_cols)	
 
 pdf("MS/fig/Bias_lnRR.pdf", height=10, width=12)
 
